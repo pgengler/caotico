e7404f88ddb2cdc5ea791ccebb139249873427ec
pgengler <pgengler@ac29549f-94df-48aa-bb8d-fee321228240>
2011-05-25 14:34:18 +0000

    Import initial Rails structure
    git-svn-id: svn+ssh://svn.pgengler.net/srv/svn/chaotic/trunk@2 ac29549f-94df-48aa-bb8d-fee321228240

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chaotic::Application.config.secret_token = 'e793bab2d7eb158aff98e0f81b724b5b6d5479324825e77bd1d6f474aaa1f27c359fc6c7a3f29c194f7b3a8c3dc367b6a7cd573e15d2c71d0b3ddd28c7145699'
