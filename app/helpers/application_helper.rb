0abafb5e2cfcbf2fdca5d18cf1b34a7b7b707362
Phil Gengler <phil@pgengler.net>
2013-01-31 15:15:37 -0500

    Add 'data-pullquote' attribute to sanitization whitelist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,15 @@ module ApplicationHelper
 	end
 
 	def markdown(text)
+		whitelist = HTML::Pipeline::SanitizationFilter::WHITELIST
+		whitelist[:attributes].merge! 'p' => [ 'data-pullquote' ]
+		context = {
+			:whitelist => whitelist
+		}
 		pipeline = HTML::Pipeline.new([
 			HTML::Pipeline::MarkdownFilter,
 			HTML::Pipeline::SanitizationFilter,
-		])
+		], context)
 		pipeline.call(text)[:output].to_s.html_safe
 	end
 
