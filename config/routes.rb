ee287732dad81032b4c38e0d2c35e2e39cc77e14
Phil Gengler <phil@pgengler.net>
2013-06-08 09:44:05 -0400

    Add 'StaticPages' controller for serving static content in the application layout, with an empty 'about' page for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Caotico::Application.routes.draw do
   resources :posts
+  resources :static_pages
   get '/posts/:id/*slug' => 'posts#show'
   get 'tags/:tag', to: 'posts#index', as: :tag
+
+  get '/about' => 'static_pages#about'
 
   root :to => "posts#index"
 end
