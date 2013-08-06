d2309fddf0ede4ee1192e31f3c5f74e61fd761a8
Phil Gengler <phil@pgengler.net>
2013-08-06 15:05:58 -0400

    Move admin-only actions from posts controller to admin/posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,43 @@
 class Admin::PostsController < ApplicationController
+  before_filter :find_post, only: [ :edit, :update, :destroy ]
+
+  def new
+    @post = Post.new
+    @title = 'New post'
+  end
+
+  def create
+    @post = Post.new(params[:post])
+
+    if @post.save
+      redirect_to post_path_with_slug(@post), notice: 'Post was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    @title = "Edit post #{@post.title}"
+  end
+
+  def update
+    if @post.update_attributes(params[:post])
+      redirect_to post_path_with_slug(@post), notice: 'Post was successfully updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_url
+  end
+
+  private
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   include PostsHelper
-  before_filter :find_post, only: [ :destroy, :edit, :show, :update ]
 
   def index
     if params[:tag]
@@ -16,45 +15,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
     @title = @post.title
   end
-
-  def new
-    @post = Post.new
-    @title = 'New post'
-  end
-
-  def create
-    @post = Post.new(params[:post])
-
-    if @post.save
-      redirect_to post_path_with_slug(@post), notice: 'Post was successfully created.'
-    else
-      render action: "new"
-    end
-  end
-
-  def edit
-    @title = "Edit post #{@post.title}"
-  end
-
-  def update
-    if @post.update_attributes(params[:post])
-      redirect_to post_path_with_slug(@post), notice: 'Post was successfully updated.'
-    else
-      render action: "edit"
-    end
-  end
-
-  def destroy
-    @post.destroy
-
-    redirect_to posts_url
-  end
-
-  private
-
-    def find_post
-      @post = Post.find(params[:id])
-    end
 end
