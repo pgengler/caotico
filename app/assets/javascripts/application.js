f7d33a973eb30464fa53a3923979d5a186b5f2cc
Phil Gengler <phil@pgengler.net>
2013-08-20 14:28:06 -0400

    Remove bootstrap-related JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-// Loads all Bootstrap javascripts
-//= require bootstrap
-$(function() {
-	$(".alert").alert();
-});
