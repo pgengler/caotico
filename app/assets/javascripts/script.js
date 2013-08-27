cacd90b373badef427dccd37b7825aaa7632c2ad
Phil Gengler <phil@pgengler.net>
2013-08-27 10:50:55 -0400

    Fix up some JS stuff

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -15,22 +15,3 @@ $('.search > .icon-remove').click(function() {
 $('.menubutton').click(function() {
 	$('header nav').slideToggle('', function() { });
 });
-
-// Responsive videos
-$(".post_video").fitVids();
-
-// Contact form validation
-$("#contact_form").validate();
-
-setTimeout(function() {
-	$("#contact_form").submit(function(e) {
-		e.preventDefault();
-		if (!$('input, textarea').hasClass('error')) {
-			$('#contact #contact_form').slideUp(250);
-			$('#contact .message').slideDown(250);
-			$.post('js/send.php', $('#contact_form').serialize());
-		} else {
-			return false;
-		}
-	});
-}, 500);
