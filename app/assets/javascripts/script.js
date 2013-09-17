d39685e5f2813183a33251c15f4a6611d02f0d51
Phil Gengler <phil@pgengler.net>
2013-09-17 11:05:15 -0400

    Set/remove focus from the search box when it's shown/hidden

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,12 +1,14 @@
 // Search popup
 $('.search > .icon-search').click(function() {
 	$('.search_popup').slideDown('', function() { });
+	$('.search_popup').find('input[type=text]').focus();
 	$('.search > .icon-search').toggleClass('active');
 	$('.search > .icon-remove').toggleClass('active');
 });
 
 $('.search > .icon-remove').click(function() {
 	$('.search_popup').slideUp('', function() { });
+	$('.search_popup').find('input[type=text]').blur();
 	$('.search > .icon-search').toggleClass('active');
 	$('.search > .icon-remove').toggleClass('active');
 });
